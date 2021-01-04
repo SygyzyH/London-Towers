ce19a38feb931f1b909e0dbcbade924462ea68c5
SygyzyH <76969579+SygyzyH@users.noreply.github.com>
2021-01-04 21:37:31 +0200

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-SCREEN_DIMENSIONS = (960, 600)
-cursor_type = "normal"
-cursor_held = False
-number_of_stages = 7
-stage_name_format = "stages/stage_$.txt"
-output_file = "log_$.txt"
-current_text = ""
-starting_hovering_ball_t = 10.466
-bouncing = False
